7f1e6031217f6b336fb425273eb9bf6e9129981f
alesboxing <77334127+alesboxing@users.noreply.github.com>
2026-06-17 11:01:04 +0400

    Override API tests database with EF InMemory

--- tests/DocFlow.Api.Tests/CustomWebApplicationFactory.cs
+++ tests/DocFlow.Api.Tests/CustomWebApplicationFactory.cs
@@ -1,5 +1,9 @@
+using DocFlow.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace DocFlow.Api.Tests;
 
@@ -8,5 +12,14 @@ public sealed class CustomWebApplicationFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<DbContextOptions<AppDbContext>>();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase($"docflow-api-tests-{Guid.NewGuid():N}");
+            });
+        });
     }
 }
